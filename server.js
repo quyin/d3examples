7c0a6bc5504ac1e88f81108b0649dbbce659dc24
Yin Qu <me.yin.qu@gmail.com>
2017-01-11 00:20:32 -0600

    add index.html. edit server.js to use index.html

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 // A simple static web server serving the current folder.
 
 var express = require('express');
-var serveIndex = require('serve-index');
 var serveStatic = require('serve-static');
 
 var app = express();
-app.use(serveIndex('.'));
 app.use(serveStatic('.'));
 
 var server = app.listen(3000, function() {
